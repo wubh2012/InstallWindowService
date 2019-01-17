51d275db18bbe6525d89764c57f25cc8fa9d7ce9
wubh2012 <wubh2012@163.com>
2019-01-17 17:41:18 +0800

    first commit

---
+++ InstallWindowService/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace InstallWindowService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.btnUnInstall = new System.Windows.Forms.Button();
+            this.txtTip = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(251, 76);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.AllowDrop = true;
+            this.txtPath.Location = new System.Drawing.Point(79, 22);
+            this.txtPath.Multiline = true;
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(434, 37);
+            this.txtPath.TabIndex = 0;
+            this.txtPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtPath_DragDrop);
+            this.txtPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtPath_DragEnter);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(333, 76);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Location = new System.Drawing.Point(79, 76);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(75, 23);
+            this.btnInstall.TabIndex = 1;
+            this.btnInstall.Text = "Install";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // btnUnInstall
+            // 
+            this.btnUnInstall.Location = new System.Drawing.Point(165, 76);
+            this.btnUnInstall.Name = "btnUnInstall";
+            this.btnUnInstall.Size = new System.Drawing.Size(75, 23);
+            this.btnUnInstall.TabIndex = 2;
+            this.btnUnInstall.Text = "UnInstall";
+            this.btnUnInstall.UseVisualStyleBackColor = true;
+            this.btnUnInstall.Click += new System.EventHandler(this.btnUnInstall_Click);
+            // 
+            // txtTip
+            // 
+            this.txtTip.Location = new System.Drawing.Point(79, 116);
+            this.txtTip.Multiline = true;
+            this.txtTip.Name = "txtTip";
+            this.txtTip.ReadOnly = true;
+            this.txtTip.Size = new System.Drawing.Size(434, 140);
+            this.txtTip.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 278);
+            this.Controls.Add(this.txtTip);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUnInstall);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Install Window Service";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.Button btnUnInstall;
+        private System.Windows.Forms.TextBox txtTip;
+    }
+}
+
---
+++ InstallWindowService/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InstallWindowService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPath_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                if (Path.GetExtension(file) == ".exe")  //判断文件类型，只接受txt文件
+                {
+                    txtPath.Text += file + "\r\n";
+                }
+            }
+        }
+
+        private void txtPath_DragEnter(object sender, DragEventArgs e)
+        {
+            txtPath.Text = "";
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] cmdline = { };
+                string serviceFileName = txtPath.Text.Trim();
+                string serviceName = GetServiceName(serviceFileName);
+                if (string.IsNullOrEmpty(serviceName))
+                {
+                    txtTip.Text = "指定文件不是Windows服务！";
+                    return;
+                }
+                if (ServiceIsExisted(serviceName))
+                {
+                    txtTip.Text = "要安装的服务已经存在！";
+                    return;
+                }
+
+                TransactedInstaller transactedInstaller = new TransactedInstaller();
+                AssemblyInstaller assemblyInstaller = new AssemblyInstaller(serviceFileName, cmdline);
+                transactedInstaller.Installers.Add(assemblyInstaller);
+                transactedInstaller.Install(new System.Collections.Hashtable());
+                txtTip.Text = "服务安装成功！";
+            }
+            catch (Exception ex)
+            {
+                txtTip.Text = ex.Message.ToString();
+            }
+
+
+        }
+
+        /// <summary>
+        /// 获取Windows服务的名称
+        /// </summary>
+        /// <param name="serviceFileName">文件路径</param>
+        /// <returns>服务名称</returns>
+        private string GetServiceName(string serviceFileName)
+        {
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(serviceFileName);
+                Type[] types = assembly.GetTypes();
+                foreach (Type myType in types)
+                {
+                    if (myType.IsClass && myType.BaseType == typeof(System.Configuration.Install.Installer))
+                    {
+                        FieldInfo[] fieldInfos = myType.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Default | BindingFlags.Instance | BindingFlags.Static);
+                        foreach (FieldInfo myFieldInfo in fieldInfos)
+                        {
+                            if (myFieldInfo.FieldType == typeof(System.ServiceProcess.ServiceInstaller))
+                            {
+                                ServiceInstaller serviceInstaller = (ServiceInstaller)myFieldInfo.GetValue(Activator.CreateInstance(myType));
+                                return serviceInstaller.ServiceName;
+                            }
+                        }
+                    }
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private bool ServiceIsExisted(string serviceName)
+        {
+            ServiceController[] services = ServiceController.GetServices();
+            foreach (ServiceController s in services)
+            {
+                if (s.ServiceName == serviceName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void btnUnInstall_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] cmdline = { };
+                string serviceFileName = txtPath.Text.Trim();
+                string serviceName = GetServiceName(serviceFileName);
+                if (string.IsNullOrEmpty(serviceName))
+                {
+                    txtTip.Text = "指定文件不是Windows服务！";
+                    return;
+                }
+                if (!ServiceIsExisted(serviceName))
+                {
+                    txtTip.Text = "要卸载的服务不存在！";
+                    return;
+                }
+                TransactedInstaller transactedInstaller = new TransactedInstaller();
+                AssemblyInstaller assemblyInstaller = new AssemblyInstaller(serviceFileName, cmdline);
+                transactedInstaller.Installers.Add(assemblyInstaller);
+                transactedInstaller.Uninstall(null);
+                txtTip.Text = "服务卸载成功！";
+
+            }
+            catch (Exception ex)
+            {
+                txtTip.Text = ex.Message;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string serviceName = GetServiceName(txtPath.Text.Trim());
+                if (string.IsNullOrEmpty(serviceName))
+                {
+                    txtTip.Text = "指定文件不是Windows服务！";
+                    return;
+                }
+                if (!ServiceIsExisted(serviceName))
+                {
+                    txtTip.Text = "要运行的服务不存在！";
+                    return;
+                }
+                ServiceController service = new ServiceController(serviceName);
+                if (service.Status != ServiceControllerStatus.Running && service.Status != ServiceControllerStatus.StartPending)
+                {
+                    service.Start();
+                    txtTip.Text = "服务运行成功！";
+                }
+                else
+                {
+                    txtTip.Text = "服务正在运行！";
+                }
+            }
+            catch (Exception ex)
+            {
+                txtTip.Text = ex.Message;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] cmdline = { };
+                string serviceFileName = txtPath.Text.Trim();
+                string serviceName = GetServiceName(serviceFileName);
+                if (string.IsNullOrEmpty(serviceName))
+                {
+                    txtTip.Text = "指定文件不是Windows服务！";
+                    return;
+                }
+                if (!ServiceIsExisted(serviceName))
+                {
+                    txtTip.Text = "要停止的服务不存在！";
+                    return;
+                }
+                ServiceController service = new ServiceController(serviceName);
+                if (service.Status == ServiceControllerStatus.Running)
+                {
+                    service.Stop();
+                    txtTip.Text = "服务停止成功！";
+                }
+                else
+                {
+                    txtTip.Text = "服务已经停止！";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                txtTip.Text = ex.Message;
+            }
+        }
+    }
+}
